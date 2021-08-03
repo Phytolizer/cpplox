ba0062b63fb863881e7646c4bc4f1c3bac3ec18c
Phytolizer <mariorules24@gmail.com>
2021-08-03 01:22:01 -0500

    fix build

--- source/token.hpp
+++ source/token.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <ostream>
+#include <stdexcept>
 #include <string>
 
+#include <fmt/core.h>
+#include <fmt/format.h>
 #include <object.hpp>
 #include <token_type.hpp>
 
@@ -20,3 +23,32 @@ struct token
 }  // namespace lox
 
 std::ostream& operator<<(std::ostream& os, const lox::token& token);
+
+template<>
+struct fmt::formatter<lox::token>
+{
+  // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+  constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin())
+  {
+    const auto* it = ctx.begin();
+    if (it != ctx.end() && *it != '}') {
+      throw format_error {"invalid format"};
+    }
+    return it;
+  }
+
+  template<typename FormatContext>
+  auto format(const lox::token& token, FormatContext& ctx)
+      -> decltype(ctx.out())
+  {
+    auto out = format_to(ctx.out(), "{} {} ", token.type, token.lexeme);
+    if (token.literal) {
+      const lox::object::value_type* ptr = &*token.literal;
+      if (const double* dp = std::get_if<double>(ptr); dp) {
+        return format_to(out, "{}", *dp);
+      }
+      throw std::logic_error {"invalid token"};
+    }
+    return format_to(out, "null");
+  }
+};
